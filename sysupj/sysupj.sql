beec342a5a9ff063de9d5065e4db0d527224d8d7
Boabat <490440749@qq.com>
2015-06-17 22:59:46 +0800

    my code
    部署服务器数据库

---
+++ sysupj/sysupj.sql
@@ -0,0 +1,57 @@
+CREATE TABLE Student
+(sid int auto_increment, 
+sname text not null check(sname != ''), 
+spwd text not null,
+PRIMARY KEY(sid));
+CREATE TABLE Teacher
+(tid int auto_increment, 
+tname text not null check(tname != ''), 
+brief text,
+PRIMARY KEY(tid));
+create table Course
+(cid int auto_increment,
+cname text not null, 
+classroom text not null, 
+capasity smallint not null, 
+courseCredit int,
+brief text,
+primary key(cid));
+create table Followings
+(id int auto_increment,
+ follower int,
+ followee int,
+primary key(id),
+foreign key(follower) references Student(sid) on delete cascade on update cascade,
+foreign key(followee) references Course(cid) on delete cascade on update cascade);
+create table Teachings
+(id int auto_increment, 
+teacher int not null, 
+course int not null,
+primary key(id),
+foreign key(teacher) references Teacher(tid) on delete cascade on update cascade,
+foreign key(course) references Course(cid) on delete cascade on update cascade);
+create table Material
+(mid int auto_increment, 
+uploader int not null, 
+course int not null, 
+filepath text not null,
+primary key(mid),
+foreign key(uploader) references Student(sid),
+foreign key(course) references Course(cid));
+create table Discussion
+(did int auto_increment, 
+poster int not null, 
+dtype int not null, 
+target int, 
+content text, 
+primary key(did),
+foreign key(poster) references Student(sid),
+foreign key(target) references Course(cid));
+create table Ratnig
+(rid int auto_increment, 
+rater int not null, 
+target int not null, 
+rating tinyint not null,
+primary key(rid),
+foreign key(rater) references Student(sid),
+foreign key(target) references Teacher(tid));
---
+++ sysupj/sysupj/models.py
@@ -0,0 +1,80 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
+from django.db import models
+
+class Student(models.Model):
+    sid = models.IntegerField(primary_key=True)
+    sname = models.TextField()
+    spwd = models.TextField()
+    def __unicode__(self):
+	    return self.sname
+    class Meta:
+        db_table = u'student'
+
+class Course(models.Model):
+    cid = models.IntegerField(primary_key=True)
+    cname = models.TextField()
+    classroom = models.TextField()
+    capasity = models.IntegerField()
+    coursecredit = models.IntegerField(null=True, db_column='courseCredit', blank=True) # Field name made lowercase.
+    brief = models.TextField(blank=True)
+    def __unicode__(self):
+	    return self.cname
+    class Meta:
+        db_table = u'course'
+
+class Teacher(models.Model):
+    tid = models.IntegerField(primary_key=True)
+    tname = models.TextField()
+    brief = models.TextField(blank=True)
+    class Meta:
+        db_table = u'teacher'
+
+class Discussion(models.Model):
+    did = models.IntegerField(primary_key=True)
+    poster = models.ForeignKey(Student, db_column='poster')
+    dtype = models.IntegerField()
+    target = models.ForeignKey(Course, null=True, db_column='target', blank=True)
+    content = models.TextField(blank=True)
+    def __unicode__(self):
+	    return self.content
+    class Meta:
+        db_table = u'discussion'
+
+class Followings(models.Model):
+    id = models.IntegerField(primary_key=True)
+    follower = models.ForeignKey(Student, null=True, db_column='follower', blank=True)
+    followee = models.ForeignKey(Course, null=True, db_column='followee', blank=True)
+    class Meta:
+        db_table = u'followings'
+
+class Material(models.Model):
+    mid = models.IntegerField(primary_key=True)
+    uploader = models.ForeignKey(Student, db_column='uploader')
+    course = models.ForeignKey(Course, db_column='course')
+    filepath = models.TextField()
+    class Meta:
+        db_table = u'material'
+
+class Ratnig(models.Model):
+    rid = models.IntegerField(primary_key=True)
+    rater = models.ForeignKey(Student, db_column='rater')
+    target = models.ForeignKey(Teacher, db_column='target')
+    rating = models.IntegerField()
+    class Meta:
+        db_table = u'ratnig'
+
+class Teachings(models.Model):
+    id = models.IntegerField(primary_key=True)
+    teacher = models.ForeignKey(Teacher, db_column='teacher')
+    course = models.ForeignKey(Course, db_column='course')
+    class Meta:
+        db_table = u'teachings'
+
